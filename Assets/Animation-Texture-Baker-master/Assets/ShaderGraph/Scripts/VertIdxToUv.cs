0c99502024109cf65baa2b6167782bdca69d1dee
saliherdogann <saliherdogan@atlasyazilim.com.tr>
2023-12-29 10:33:13 +0300

    fixed

--- Assets/Animation-Texture-Baker-master/Assets/ShaderGraph/Scripts/VertIdxToUv.cs
+++ Assets/Animation-Texture-Baker-master/Assets/ShaderGraph/Scripts/VertIdxToUv.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 
 [RequireComponent(typeof(MeshFilter))]
 public class VertIdxToUv : MonoBehaviour
@@ -10,6 +11,11 @@ public class VertIdxToUv : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+    }
+
+    [ContextMenu("bake vertices")]
+    public void BakeVertIdxToUv()
+    {
         var mesh = GetComponent<MeshFilter>().mesh;
         mesh.hideFlags = HideFlags.DontSave;
         var uv = new List<Vector2>();
@@ -17,5 +23,35 @@ public class VertIdxToUv : MonoBehaviour
         for (var i = 0; i < count; i++)
             uv.Add(new Vector2(i + 0.5f, 0) / Mathf.NextPowerOfTwo(count));
         mesh.SetUVs(channel, uv);
+        SaveMesh(mesh, "NewInstance", false, false);
+    }
+    public static void SaveMeshInPlace(MenuCommand menuCommand)
+    {
+        MeshFilter mf = menuCommand.context as MeshFilter;
+        Mesh m = mf.sharedMesh;
+        SaveMesh(m, m.name, false, true);
+    }
+
+    public static void SaveMeshNewInstanceItem(MenuCommand menuCommand)
+    {
+        MeshFilter mf = menuCommand.context as MeshFilter;
+        Mesh m = mf.sharedMesh;
+        SaveMesh(m, m.name, true, true);
+    }
+
+    public static void SaveMesh(Mesh mesh, string name, bool makeNewInstance, bool optimizeMesh)
+    {
+        string path = EditorUtility.SaveFilePanel("Save Separate Mesh Asset", "Assets/", name, "asset");
+        if (string.IsNullOrEmpty(path)) return;
+
+        path = FileUtil.GetProjectRelativePath(path);
+
+        Mesh meshToSave = (makeNewInstance) ? Object.Instantiate(mesh) as Mesh : mesh;
+
+        if (optimizeMesh)
+            MeshUtility.Optimize(meshToSave);
+
+        AssetDatabase.CreateAsset(meshToSave, path);
+        AssetDatabase.SaveAssets();
     }
 }
